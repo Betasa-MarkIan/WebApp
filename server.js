3b0b4976c0c06a5d916b1a22bb95939765e68895
Betasa-MarkIan <betasa.gutierrezmarkian@gmail.com>
2025-01-24 15:24:27 +0800

    deployment

--- server.js
+++ server.js
@@ -17,51 +17,40 @@ const workoutPlanRoutes = require('./routes/workoutplans')
 
 //global middleware
 app.use(express.json())
-app.use(cors({origin: 'http://localhost:5173'}))
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // routes
-// app.use('/api/workouts', workoutRoutes)
-// app.use('/api/exercises', exerciseRoutes)
-// app.use('/api/workoutplans', workoutPlanRoutes)
-
-//for testing
-app.get('/', (req, res)=> {
-  res.send("server is running")
-})
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http:localhost:${PORT}`)
-})
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/exercises', exerciseRoutes)
+app.use('/api/workoutplans', workoutPlanRoutes)
 
 
-// Connect to db - async 
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     // listen for request
-//     app.listen(process.env.PORT, () => { 
-//     console.log('connected to db...')
-//     console.log('listening on port 4000')
-//    })
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
 
-  // mongoose.
-  //   connect(process.env.MONGO_URI, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   })
-  //   .then(() => console.log("MongoDB connected...listening on port 4000"))
-  //   .catch((error) => {
-  //     console.error("MongoDB Connection error:", error.message)
-  //     process.exit(1)
-  //   })
+//Connect to db - async 
+mongoose.connect(process.env.MONGO_URI, {  
+  useNewUrlParser: true, 
+  useUnifiedTopology: true })
+  .then(() => {
+    app.listen(process.env.PORT, () => { 
+    console.log('connected to db...')
+    console.log('listening on port 5000')
+   })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 
-  
+const path = require("path");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "dist"))); // Adjust path if necessary
+
+// Catch-all handler to serve React's index.html for unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
